f923617bb06fd2d645f16ba862a1b90def373ebe
lots-o <kgh0916@gmail.com>
2021-01-19 17:50:39 +0900

    Update my code

--- text_processing2.py
+++ text_processing2.py
@@ -28,8 +28,13 @@ def digits_to_words(input_string):
             >>> tp2.digits_to_words(digits_str2)
             'three one four one five'
     """
-    digit_string = None
-    return digit_string
+    
+    digit_string = []
+    convertor={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+    digit_string=[convertor[int(char)] for char in input_string if char.isdigit()]
+
+    
+    return ' '.join(digit)
 
 
 """
@@ -43,7 +48,7 @@ def digits_to_words(input_string):
 def to_camel_case(underscore_str):
     """
     이 문제에서 첫번째 규칙 'underscore variable' 에서 두번째 규칙 'camelcase variable'으로 변환함
-    * 앞과 뒤에 여러개의 'underscore'는 무시해도 된
+    * 앞과 뒤에 여러개의 'underscore'는 무시해도 된다.
     * 만약 어떤 변수 이름이 underscore로만 이루어 진다면, 빈 문자열만 반환해도 됨
 
         Parameters:
@@ -65,4 +70,17 @@ def to_camel_case(underscore_str):
             "alreadyCamel"
     """
     camelcase_str = None
+    splited_str=underscore_str.split('_')
+    start=True
+    for char in splited_str:
+        if char == '':continue
+        
+        if start :
+            camelcase_str+=char
+            start=False
+        else:
+            camelcase_str+=char.capitalize()
+            
+        
+    
     return camelcase_str
